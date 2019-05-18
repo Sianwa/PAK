f6831d6fc6150d297baa0cc2587677932ca052db
Sianwa <32924825+Sianwa@users.noreply.github.com>
2019-05-18 18:40:15 +0300

    connect to firebase

---
+++ Projo/js/firebase.js
@@ -0,0 +1,19 @@
+var app_firebase={};
+(function(){
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDquApmcOMA9xeQYzv-6-tGhMv-Q8HyPxs",
+    authDomain: "fir-web-learn-bb47c.firebaseapp.com",
+    databaseURL: "https://fir-web-learn-bb47c.firebaseio.com",
+    projectId: "fir-web-learn-bb47c",
+    storageBucket: "fir-web-learn-bb47c.appspot.com",
+    messagingSenderId: "196632649864",
+    appId: "1:196632649864:web:d6f9ecdaf3b4a058"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+app_firebase = firebase;
+})()
+
