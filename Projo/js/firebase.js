29010f8ebf3f81fd45ca9fadec1e5fd041c68cd5
Sianwa <32924825+Sianwa@users.noreply.github.com>
2019-05-25 23:59:12 +0300

    h

--- Projo/js/firebase.js
+++ Projo/js/firebase.js
@@ -5,11 +5,8 @@ const auth =firebase.auth();
         var password = document.getElementById("inputPassword").value;
         var pas = document.getElementById("inputPassword").value;
         var pass = document.getElementById("pass").value;
-
-        //var name = document.getElementById("name").value;
        
-
-        
+ 
 if (pas != pass){
             window.alert("Passwords do not match");
                 } 
@@ -20,34 +17,41 @@ if (pas != pass){
                  else{
                         auth.createUserWithEmailAndPassword(email, password).then( user => {
                         if(user) {
-                             window.location = 'login.html'; //After successful login, user will be redirected to home.html
+                             window.location = 'login.html'; 
                                  }
-}).catch(function(error) {
-            // Handle Errors here.
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            window.alert("Error " +errorMessage)
-});
-        }   
+                       }).catch(function(error) {
+                         // Handle Errors here.
+                            var errorCode = error.code;
+                            var errorMessage = error.message;
+                            window.alert("Error " +errorMessage)
+                });
+                    }   
  }
 
 // This is for log in
 function Login(){
-  window.alert("hello");
+  
     var email = document.getElementById("inputEmail").value;
     var password = document.getElementById("inputPassword").value;
         
-    auth.signInWithEmailAndPassword(email, password).then( user => {
-    if(user) {
-    window.location.href = 'landingPage.html'; //After successful login, user will be redirected to home.html
-    }else{
-      window.alert("firebase failes")
-    }
-}).catch(function(error) {
+    auth.signInWithEmailAndPassword(email, password)
+    
+    //window.alert("hello");
+    //window.location.href = 'landingPage.html'; //After successful login, user will be redirected to home.html
+    
+.catch(function(error) {
   // Handle Errors here.
   var errorCode = error.code;
   var errorMessage = error.message;
-        window.alert("Error " +errorMessage)
+
+  if(errorCode == 'auth/invalid-email'|| 'auth/wrong-password')
+  {
+    console.log('Invalid Credentials');
+  }
+   else
+   {
+    console.log('Error :' +errorCode);
+   }     
   // ...
 });
 }
