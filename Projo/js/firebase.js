d8d273bc2dd1e0660f94c903bda1a0f60d2ac24a
Sianwa <32924825+Sianwa@users.noreply.github.com>
2019-05-25 22:46:20 +0300

    m

--- Projo/js/firebase.js
+++ Projo/js/firebase.js
@@ -1,36 +1,44 @@
-function register(){
-
-        var email = document.getElementById("inputEmail").value;
-        var password = document.getElementById("inputPassword").value;
-        var pas = document.getElementById("inputPassword").value;
+ function register(){
+        var email = document.getElementById("email").value;
+        var password = document.getElementById("pas").value;
+        var pas = document.getElementById("pas").value;
         var pass = document.getElementById("pass").value;
+        //var name = document.getElementById("name").value;
+       
 
+        
 if (pas != pass){
-                 window.alert("Passwords do not match");
-                }
-        else if (pas.length < 8){
-            window.alert("Passwords must have more than 8 characters");
-              }
-          else
-          {
-        firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // ...
+            window.alert("Passwords do not match");
+                } 
+    else if (pas.length < 8)
+                {
+             window.alert("Passwords must have more than 8 characters");
+        }
+        else{
+            firebase.auth().createUserWithEmailAndPassword(email, password).then( user => {
+    if(user) {
+         window.location = 'login.html'; //After successful login, user will be redirected to home.html
+    }
+}).catch(function(error) {
+            // Handle Errors here.
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            window.alert("Error " +errorMessage)
 });
-        } 
-   
-}
+        }   
+ }
 
 // This is for log in
 function Login(){
+  window.alert("hello");
     var email = document.getElementById("inputEmail").value;
     var password = document.getElementById("inputPassword").value;
         
     firebase.auth().signInWithEmailAndPassword(email, password).then( user => {
     if(user) {
     window.location.href = 'landingPage.html'; //After successful login, user will be redirected to home.html
+    }else{
+      window.alert("firebase failes")
     }
 }).catch(function(error) {
   // Handle Errors here.
