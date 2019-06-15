e85bf7406ea9f58dd1b04d607b3bc0298fff2949
Brok <brianokoth11@gmail.com>
2019-06-15 16:28:34 +0300

    editing the app.js and editacc form

--- Projo/js/app.js
+++ Projo/js/app.js
@@ -1,6 +1,27 @@
 //listen for submit event//
 document.getElementById('EditAcc').addEventListener('submit', formSubmit);
 
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyCeSljgY83YrGTifHqFExwB92j85Wl1aso",
+    authDomain: "pak-hci.firebaseapp.com",
+    databaseURL: "https://pak-hci.firebaseio.com",
+    projectId: "pak-hci",
+    storageBucket: "pak-hci.appspot.com",
+    messagingSenderId: "714721382084",
+    appId: "1:714721382084:web:ce0e4e9e51394a10"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+let formMessage = firebase.database().ref('edit');
+
+//listen for submit event//
+document
+  .getElementById('EditAcc')
+  .addEventListener('submit', formSubmit);
+
 //Submit form
 function formSubmit(e) {
   e.preventDefault();
@@ -13,3 +34,22 @@ function formSubmit(e) {
   let uname = document.querySelector('#uname').value;
   let pass = document.querySelector('#pass').value;
   let cpass = document.querySelector('#cpass').value;
+    //send message values
+  sendMessage(fname, lname, com, bio, email, uname, pass, cpass);
+}
+
+function sendMessage(fname, lname, com, bio, email, uname, pass, cpass) {
+  let newFormMessage = formMessage.push();
+  newFormMessage.set({
+    fname: fname,
+    lname: lname,
+    com: com,
+    bio: bio,
+    email: email,
+    uname: uname,
+    pass: pass,
+    cpass: cpass
+  });
+    
+}
+ 
