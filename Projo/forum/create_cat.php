625b39ac1f123e7e1c73bbda0a4de4a49148ef62
Briskwoods <julian.gichuki@gmail.com>
2019-06-15 21:54:36 +0300

    Updated User Levels
    Made it that only admins can access the change category page, to prevent spam on the site. Will find a way to completely hide button to normal users.

--- Projo/forum/create_cat.php
+++ Projo/forum/create_cat.php
@@ -4,26 +4,27 @@ include 'header.php';
     
     echo '<h2>Create a Category</h2>';
     
-    
+    //if a user is signed in 
     if(isset($_SESSION['signed_in']) && $_SESSION['signed_in'] == true)
     { 
-        //if a user is signed in      
-        if($_SERVER['REQUEST_METHOD'] != 'POST')  
-        {  
+        if (isset($_SESSION['user_level']) && $_SESSION['user_level']=="1")
+        {              
+            if($_SERVER['REQUEST_METHOD'] != 'POST')  
+            {
             //the form hasn't been posted yet, display it    
             echo '<form method="post" action="">
             <table align= "centre">
-              <tr>        
-              <td>Category name: <br><input type="text" name="cat_name" /></td>
-              </tr>
-              <tr>
-              <td>Category description: <br><textarea name="cat_description" /></textarea></td>
-              </tr>
-              <tr>
-              <td><input type="submit" value="Add category" /></td>
-              </tr>
-              </table>
-              </form>';
+            <tr>        
+            <td>Category name: <br><input type="text" name="cat_name" /></td>
+            </tr>
+            <tr>
+            <td>Category description: <br><textarea name="cat_description" /></textarea></td>
+            </tr>
+            <tr>
+            <td><input type="submit" value="Add category" /></td>
+            </tr>
+            </table>
+            </form>';
             }
             else
             {                
@@ -33,20 +34,23 @@ include 'header.php';
                 if(!$result)
                 {
                     //something went wrong, display the error
-                    echo 'Error' . mysqli_error($con);
+                    echo 'Error' . mysqli_error($con);            
                 }
                 else
                 {
                     echo 'New category successfully added.';
                 }
             }
-        }        
-        else
-        {
-            //the user is not signed in
-            echo 'Sorry, you have to be <a href="/Projo/forum/signin.php">signed in</a> to create a topic.';
         }
-
+        else{
+            echo 'Only Admins are allowed to create a topic.';
+        }
+    }
+    else
+    {
+        //the user is not signed in     
+        echo 'Sorry, you have to be <a href="/Projo/forum/signin.php">signed in</a> to create a topic.';            
+    }
 
 
 include 'footer.php';
