e996120836988d0e5807726e6aac3f98cd283097
Briskwoods <julian.gichuki@gmail.com>
2019-06-16 14:59:01 +0300

    Updated Database

--- Projo/forumDB/forum.sql
+++ Projo/forumDB/forum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 27, 2019 at 12:49 AM
+-- Generation Time: Jun 16, 2019 at 01:56 PM
 -- Server version: 10.1.38-MariaDB
 -- PHP Version: 7.3.3
 
@@ -40,8 +40,9 @@ CREATE TABLE `categories` (
 
 INSERT INTO `categories` (`cat_id`, `cat_name`, `cat_description`) VALUES
 (6, 'Rules', 'Forum Rules:\r\nNo Self-Advertising\r\nNo Spamming\r\netc\r\n'),
-(7, 'Members', 'All Current Members in our Community\r\n'),
-(9, 'Fam', 's');
+(10, 'Requests', 'Where users can request for new categories to be added by admins.\r\nThis is admin controlled to prevent spam.'),
+(14, 'Suggestions', 'Add suggestions on how we can improve our site and discuss with one another how we can make our services and website better. Thank you.'),
+(15, 'Discussions', 'Connect with other photographers, create threads, communicate and work together.');
 
 -- --------------------------------------------------------
 
@@ -66,19 +67,14 @@ INSERT INTO `posts` (`post_id`, `post_content`, `post_date`, `post_topic`, `post
 (9, 'Rules Suck', '2019-05-26 22:39:26', 3, 3),
 (10, 'Yeet', '2019-05-26 22:41:18', 3, 3),
 (12, 'Finally\r\n', '2019-05-26 23:19:28', 3, 3),
-(13, 'Lader Hosen', '2019-05-26 23:26:47', 5, 3),
 (14, 'Increasing the rules boiis', '2019-05-27 00:33:21', 6, 3),
 (15, 'SSSSSSSSs', '2019-05-27 00:33:38', 3, 3),
 (24, 'SSSSSSSS', '2019-05-27 01:04:51', 6, 3),
-(25, 'a', '2019-05-27 01:05:51', 7, 3),
-(26, 'AAA', '2019-05-27 01:05:56', 7, 3),
-(27, 'sss', '2019-05-27 01:08:02', 8, 4),
 (28, 'Hello', '2019-05-27 01:10:55', 3, 4),
 (29, 'he', '2019-05-27 01:11:22', 3, 4),
-(30, 'sss', '2019-05-27 01:30:52', 9, 3),
-(31, 's', '2019-05-27 01:31:15', 9, 3),
-(32, 'a', '2019-05-27 01:31:36', 9, 4),
-(33, 's', '2019-05-27 01:31:47', 9, 4);
+(36, 'Kindly add a report feature in future to protect users from harassment online.', '2019-06-15 22:28:17', 11, 6),
+(37, 'Could you add a member list to the website?', '2019-06-15 22:29:26', 12, 6),
+(39, 'Welcome', '2019-06-15 22:30:07', 14, 6);
 
 -- --------------------------------------------------------
 
@@ -100,11 +96,10 @@ CREATE TABLE `topics` (
 
 INSERT INTO `topics` (`topic_id`, `topic_subject`, `topic_date`, `topic_cat`, `topic_by`) VALUES
 (3, 'Rule 1', '2019-05-20 09:37:49', 6, 3),
-(5, 'VIP', '2019-05-26 23:26:47', 7, 3),
 (6, 'More Rules', '2019-05-27 00:33:21', 6, 3),
-(7, 'WAA', '2019-05-27 01:05:51', 6, 3),
-(8, 'sss', '2019-05-27 01:08:02', 6, 4),
-(9, 'sss', '2019-05-27 01:30:52', 9, 3);
+(11, 'Reports Widget', '2019-06-15 22:28:17', 14, 6),
+(12, 'Member List', '2019-06-15 22:29:26', 10, 6),
+(14, 'New Users', '2019-06-15 22:30:07', 15, 6);
 
 -- --------------------------------------------------------
 
@@ -127,7 +122,9 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`user_id`, `user_name`, `user_pass`, `user_email`, `user_date`, `user_level`) VALUES
 (3, 'Ace', 'a6bfc35250672f309829b503a7d8cde05c3b7f4d', 'firefist@gmail.com', '2019-05-18 13:52:56', 1),
-(4, 'James', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'jamesbay@abc.com', '2019-05-27 01:07:30', 0);
+(4, 'James', '7c4a8d09ca3762af61e59520943dc26494f8941b', 'jamesbay@abc.com', '2019-05-27 01:07:30', 0),
+(5, 'admin', 'd033e22ae348aeb5660fc2140aec35850c4da997', 'admin@pak.co.ke', '2019-06-15 18:10:47', 1),
+(6, 'user1', 'b3daa77b4c04a9551b8781d03191fe098f325e67', 'user1@pak.co.ke', '2019-06-15 22:26:46', 0);
 
 --
 -- Indexes for dumped tables
@@ -171,25 +168,25 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `categories`
 --
 ALTER TABLE `categories`
-  MODIFY `cat_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `cat_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `post_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `post_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT for table `topics`
 --
 ALTER TABLE `topics`
-  MODIFY `topic_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `topic_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `user_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_id` int(8) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
